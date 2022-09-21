5fb1b681e881a6244bcc9e52567b1bcbfa13a329
Denis Yakshov <feerzlay@gmail.com>
2022-09-21 19:07:26 +0300

    feat: setup git hooks

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'style', 'test', 'chore', 'recipe']
+    ],
+    'header-max-length': [0, 'always']
+  }
+};
